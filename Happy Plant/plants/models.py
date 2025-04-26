fb46ccc8de0f83f69a2d84d75243c443d6c3b44e
student <student@BTU.local>
2025-04-26 17:32:50 +0400

    some fix

--- Happy Plant/plants/models.py
+++ Happy Plant/plants/models.py
@@ -3,26 +3,26 @@ from users.models import User
 from django import forms
 from django.utils import timezone
 
-class PlantType(models.Model):
+class PlantType(models.Model): #just plant types
     latin_name = models.CharField(max_length=255)
     family = models.CharField(max_length=255)
-    common_name = models.JSONField(default=list)  # Используем JSONField для списка
+    common_name = models.JSONField(default=list) 
     category = models.CharField(max_length=255)
     origin = models.CharField(max_length=255)
     climate = models.CharField(max_length=255)
-    temp_max = models.JSONField(default=dict)  # Используем JSONField для словаря
-    temp_min = models.JSONField(default=dict)  # Используем JSONField для словаря
+    temp_max = models.JSONField(default=dict) 
+    temp_min = models.JSONField(default=dict) 
     ideal_light = models.CharField(max_length=255)
     tolerated_light = models.CharField(max_length=255)
     watering = models.CharField(max_length=255)
-    insects = models.JSONField(default=list)  # Используем JSONField для списка
+    insects = models.JSONField(default=list)  
     diseases = models.CharField(max_length=255)
     use = models.JSONField(default=list) 
 
     def __str__(self):
         return f'{self.common_name}'
 
-class Plant(models.Model):
+class Plant(models.Model): #user's plant
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     plant_type = models.ForeignKey(PlantType, on_delete=models.CASCADE)
     
@@ -35,7 +35,7 @@ class Plant(models.Model):
         return f"{self.name} ({self.species})"
 
 
-class PlantForm(forms.ModelForm):
+class PlantForm(forms.ModelForm): #form for fill
     class Meta:
         model = Plant
         fields = ['name', 'plant_type', 'species', 'age']
